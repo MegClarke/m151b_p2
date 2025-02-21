e4d4cc7737f54ea4186de83059f11029f6b86bb9
Meagan Clarke <megclarke@g.ucla.edu>
2025-02-21 02:33:01 -0500

    updated gshare.h & gshare.cpp

--- src/gshare.cpp
+++ src/gshare.cpp
@@ -24,7 +24,7 @@ using namespace tinyrv;
 ///////////////////////////////////////////////////////////////////////////////
 
 GShare::GShare(uint32_t BTB_size, uint32_t BHR_size)
-  : BTB_(BTB_size, BTB_entry_t{false, 0x0, 0x0})
+  : BTB_(BTB_size, BTB_entry_t{false, 0x0, 0x0})   //meagan: I initialized this struct to be {valid, target, tag} not sure if thats correct
   , PHT_((1 << BHR_size), 0x0)
   , BHR_(0x0)
   , BTB_shift_(log2ceil(BTB_size))
@@ -41,7 +41,20 @@ uint32_t GShare::predict(uint32_t PC) {
   uint32_t next_PC = PC + 4;
   bool predict_taken = false;
 
-  // TODO:
+  // TODO: meagan: i think mostly done
+  //2 bits of the PC are always 0, cuz instruction alignment bits (mentioned on campus wire) so we can shift to get the meaningful bits
+  uint32_t BTB_index = (PC >> 2) & BTB_mask_;
+  
+  uint32_t PHT_index = (PC >> 2) ^ BHR_; //xor the PC with the BHR to get the index into the PHT
+  predict_taken = PHT_[PHT_index] > 1; //if the value in the PHT is greater than 1, predict taken
+
+  if (predit_taken && BTB_[BTB_index].valid) { //if the BTB says we should branch
+    next_PC = BTB_[BTB_index].target; //then branch to the target address
+    //what are the tags for????? and also what to do if not valid (zane please test and figure this out)
+  }
+  //it predit_taken is false, then we just go to the next PC (which is PC + 4) so don't change next PC
+  
+
 
   DT(3, "*** GShare: predict PC=0x" << std::hex << PC << std::dec
         << ", next_PC=0x" << std::hex << next_PC << std::dec
@@ -54,7 +67,28 @@ void GShare::update(uint32_t PC, uint32_t next_PC, bool taken) {
         << ", next_PC=0x" << std::hex << next_PC << std::dec
         << ", taken=" << taken);
 
-  // TODO:
+  // TODO: meagan: this is not done but i think the last thing to do is just update valid, and tags, but I don't understand them
+  // Update the BHR 
+  BHR_ = ((BHR_ << 1) | (taken ? 1 : 0)) & BHR_mask_; //shift left and add the taken bit to the end & by 0b11111111
+
+  // Update the PHT (if actually taken, increment by 1, if actually not taken, decrement by 1)
+  uint32_t PHT_index = (PC >> 2) ^ BHR_; 
+  if (taken) {
+    if (PHT_[PHT_index] < 3) {
+      PHT_[PHT_index]++;
+    }
+  } else {
+    if (PHT_[PHT_index] > 0) {
+      PHT_[PHT_index]--;
+    }
+  }
+
+  // Update the BTB
+  uint32_t BTB_index = (PC >> 2) & BTB_mask_;
+  BTB_[BTB_index].valid = true; //it's been visited / used before
+  BTB_[BTB_index].target = next_PC; //could be PC+4 if not taken or the branch target but we shouldn't have to handle the conditional
+  //what is the tag for?????
+  
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- src/gshare.h
+++ src/gshare.h
@@ -32,6 +32,13 @@ public:
   };
 };
 
+//there was no TODO here but campuswire says to initialize this struct so i did
+struct BTB_entry_t {
+    bool valid;        // what does valid mean exactly? my guess is it means that it has been used before
+    uint32_t target;   // target address
+    uint32_t tag;      // tag to differentiate branches mapping to the same index (don't really understand this yet)
+};
+
 class GShare : public BranchPredictor {
 public:
   GShare(uint32_t BTB_size, uint32_t BHR_size);
@@ -41,7 +48,24 @@ public:
   uint32_t predict(uint32_t PC) override;
   void update(uint32_t PC, uint32_t next_PC, bool taken) override;
 
-  // TODO: Add your own methods here
+  // TODO: Add your own methods here (meagan: done, but added a lot of comments to explain what's going on for the .cpp file implementation)
+  private:
+    std::vector<BTB_entry_t> BTB_;     // Branch Target Buffer (if PHT says taken, then this stores the target address to branch to)
+    // this is of size BTB_size (which I think is 256), I think this is indexed by just some 8 bits of the PC (NO XOR) - not sure which 8 (but my guess is bits 10 to 2)
+    //  how to use the tag???
+    std::vector<uint8_t> PHT_;         // Pattern History Table (this determines whether we predict taken or not) - initialized to 0
+    //okay so this is confusing because the readme talks about having a BHT (Branch History Table) but the code has a PHT, I'm assuming they're the same thing
+    //automatically of size 2^(BHR_size = 8) = 256 and initialized to 0
+    //I'm assuming that the PHT is indexed by PC XOR BHR -> still figuring out which bits of the PC (but my guess is bits 10 to 2)
+    //if this is a 2-bit predictor (im assuming but it's not specified) we only use last 2 bits of uint_8_t entry to make the prediction 
+    //00: Strongly not taken, 01: Weakly not taken, 10: Weakly taken, 11: Strongly taken (implemented)
+    //if actually taken, increment by 1, if actually not taken, decrement by 1 (implement)
+
+    uint32_t BHR_;                     // Branch History Register (8 bits)
+    uint32_t BTB_shift_;               // genuinely not sure what this is for
+    uint32_t BTB_mask_;                // this is literally just 0b11111111 so that we can zero out everything except last 8 bits of the PC
+    uint32_t BHR_mask_;                // this is literally just 0b11111111 so that we can zero out everything except last 8 bits of the BHR
+
 };
 
 class GSharePlus : public BranchPredictor {
